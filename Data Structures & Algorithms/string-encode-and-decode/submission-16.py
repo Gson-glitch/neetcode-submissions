6679905622bbcf3cda522b271f3a7a0fcb5e2ab7
Gloryson Ondanje <59365410+Gson-glitch@users.noreply.github.com>
2026-08-26 14:17:39 +0300

    Add: string-encode-and-decode - submission-16

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-16.py
@@ -0,0 +1,30 @@
+import re
+
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        encoded_str = ""
+        sep = "#"
+        for s in strs:
+            len_s = len(s)
+            encoded_str += str(len_s) + sep
+            encoded_str += s
+        return encoded_str
+
+    def decode(self, s: str) -> List[str]:
+        sep = "#"
+        pattern = re.compile(rf"(\d+){re.escape(sep)}")
+        res = []
+        i = 0
+
+        while i < len(s):
+            match = pattern.match(s, i)
+            length = int(match.groups()[0])
+            end = match.end()
+            res.append(s[end: end+length])
+            i = end+length
+
+        return res
+
+            
+        
+
